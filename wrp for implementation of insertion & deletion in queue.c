5dc45e0d5a1744385084f5f1962b1dae4c5fa6ca
subhankar pahi <110842641+subhankar-pahi-2005@users.noreply.github.com>
2022-10-26 20:19:52 +0530

    Update wrp for implementation of insertion & deletion in queue.c

--- wrp for implementation of insertion & deletion in queue.c
+++ wrp for implementation of insertion & deletion in queue.c
@@ -1,85 +1,91 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 #define MAX 50
-
+ 
 void insert();
 void delete();
 void display();
 int queue_array[MAX];
-int rear=-1;
-int front=-1;
+int rear = - 1;
+int front = - 1;
 int main()
 {
-	int choice;
-	printf("*****WELCOME TO QUEUE PROGRAM*****");
-	while(1)
-{
-printf("\n1.INSERT ELEMENT TO INSERT IN QUEUE\n");
-printf("\n2.DELETE ELEMENT FROM QUEUE\n");
-printf("\n3.DISPLAY ALL ELEMENTS OF QUEUE\n");
-printf("4.QUIT\n");
-printf("ENTER YOUR CHOICE: ");
-scanf("%d",choice);
-switch(choice)
-{
-	    case 1:
-		insert();
-		break;
-		case 2:
-		delete();
-		break;
-		case 3:
-		display();
-		break;
-		case 4:
-		exit(1);
-		default:
-		printf("WRONG CHOICE\n");
-		}
-	}
-}
+    int choice;
+    printf("******WELCOME TO QUEUE PROGRAM******");
+    while (1)
+    {
+    	printf("               \n                          ");
+        printf("\n=>>1.Insert element to queue \n");
+        printf("-------------------------------\n");
+        printf("=>>2.Delete element from queue \n");
+        printf("-------------------------------\n");
+        printf("=>>3.Display all elements of queue \n");
+        printf("-------------------------------\n");
+        printf("=>>4.Quit \n");
+        
+        printf("           \n                        ");
+        printf("\nEnter your choice==>> ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+            insert();
+            break;
+            case 2:
+            delete();
+            break;
+            case 3:
+            display();
+            break;
+            case 4:
+            exit(1);
+            default:
+            printf("Wrong choice \n");
+        } 
+    } 
+} 
+ 
 void insert()
 {
-	int item;
-	if(rear==MAX-1)
-	printf("QUEU OVERFLOW\n");
-	else
-{
-if(front==-1)
-{
-front=0;
-printf("\nINSERT ELEMENT TO QUEUE: ");
-scanf("%d",&item);
-rear=rear+1;
-queue_array[rear]=item;
-}
-}
-}
+    int add_item;
+    if (rear == MAX - 1)
+    printf("Queue Overflow \n");
+    else
+    {
+        if (front == - 1)
+       
+        front = 0;
+        printf("Inset the element in queue : ");
+        scanf("%d", &add_item);
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+    }
+} 
+ 
 void delete()
 {
-	if(front==-1 || front>rear)
-{
-printf("QUEUE UNDERFLOW\n");
-return;
-}
-else
-{
-	printf("ELEMENT DELETE FROM QUEUE IS :%d\n",queue_array[front]);
-	front=front+1;
-}
-}
+    if (front == - 1 || front > rear)
+    {
+        printf("Queue Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from queue is : %d\n", queue_array[front]);
+        front = front + 1;
+    }
+} 
+ 
 void display()
 {
-	int i;
-	if(front==-1)
-	printf("QUEUE IS EMPTY\n");
-	else
-{
-printf("QUEUE IS :\n");
-for(i=front;i<=rear;i++)
-{
-printf("%d",queue_array[i]);
-printf("\n");
-}
-}
-}
+    int i;
+    if (front == - 1)
+        printf("Queue is empty \n");
+    else
+    {
+        printf("Queue is : \n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+    }
+} 
